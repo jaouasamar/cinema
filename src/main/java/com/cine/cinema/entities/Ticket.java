7f32d126c398c7f6c9dbcf1f7de450f2d3958d64
samar jaoua <jaoua.samar@gmail.com>
2021-10-12 18:24:21 +0100

    first commit

---
+++ src/main/java/com/cine/cinema/entities/Ticket.java
@@ -0,0 +1,36 @@
+package com.cine.cinema.entities;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Data @AllArgsConstructor @NoArgsConstructor @ToString
+public class Ticket {
+
+	@Id 
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	@Column(length=75)
+	private String nomClient;
+	private double prix;
+	@Column(unique=true)
+	private int codePayement;
+	private boolean reserve;
+	@ManyToOne
+	private Place place;
+	
+	@ManyToOne
+	private Projection projection;
+	
+}
